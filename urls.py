a6eac1b2701ae881a7ce10d54e4927ad0e75bc3b
junho04 <whwnsghsla@naver.com>
2025-06-09 22:35:04 +0900

    display_info
    display_info

--- urls.py
+++ urls.py
@@ -1,26 +1,8 @@
-"""
-URL configuration for mypersonalwebsite project.
+# display_info/urls.py
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-# mypersonalwebsite/urls.py
-
-from django.contrib import admin
-from django.urls import path, include # include 추가
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('display_info.urls')), # 여기에 추가
+    path('', views.index, name='index'),
 ]
